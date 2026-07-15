bcad2b12cb10aa29b620501d81e96c1b3a30b238
Md. Anik Chowdhury <anikchowdhury262000@gmail.com>
2026-07-15 15:38:09 +0600

    Clean up print statements in ddl_bronze.sql
    Removed unnecessary print statements for better readability.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -8,9 +8,7 @@ BEGIN
 DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
 BEGIN TRY
 SET @batch_start_time = GETDATE();
-PRINT'=======================================================';
 PRINT 'Loading Bronze Layer';
-PRINT'=======================================================';
 PRINT 'LOADING CRM TABLES'
 
 SET @start_time = GETDATE();
@@ -114,17 +112,13 @@ WITH (
 );
 
 SET @batch_end_time = GETDATE();
-		PRINT '=========================================='
 		PRINT 'Loading Bronze Layer is Completed';
         PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
-		PRINT '=========================================='
 END TRY
 BEGIN CATCH
-PRINT'========================================================='
 PRINT'ERROR THE CODE DURING LOADING BRONZE LAYER'
 PRINT 'Error Message' + ERROR_MESSAGE();
 PRINT 'Error Message' + CAST (ERROR_NUMBER() AS NVARCHAR);
-PRINT'========================================================='
 END CATCH
 END
 
